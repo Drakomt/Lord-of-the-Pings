b24b6673ffdc6da991538639163f75d3874bff32
Drakomt <mati.tsiplakov@gmail.com>
2026-01-21 11:53:36 +0200

    Changed server UI

--- Part_2/server.py
+++ Part_2/server.py
@@ -478,8 +478,7 @@ def server_thread():
     server_socket.bind((SERVER_HOST, SERVER_PORT))
     server_socket.listen()
 
-    log(f"Server port: {SERVER_PORT}")
-    log(f"Discovery port: {DISCOVERY_PORT}")
+    update_server_info_label()
     log(f"[*] Server listening on {SERVER_HOST}:{SERVER_PORT}")
 
     while True:
@@ -503,6 +502,32 @@ app.configure(fg_color=BACKGROUND_COLOR)
 icon_path = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "assets/icons/Lotp_Icon_BP.ico")
 app.iconbitmap(icon_path)
+
+# ----- Server Info Labels -----
+frame_top = ctk.CTkFrame(app, corner_radius=15,
+                         fg_color=BACKGROUND_COLOR, border_width=0)
+frame_top.pack(fill="x", padx=10, pady=(10, 5))
+
+server_host_frame = ctk.CTkFrame(
+    frame_top, fg_color=OTHER_COLOR, corner_radius=8, border_color=ACCENT_COLOR, border_width=1)
+server_host_frame.pack(side="left", padx=5, pady=10, fill="x", expand=True)
+server_host_label = ctk.CTkLabel(server_host_frame, text="", font=("Arial", 11, "bold"), text_color=TEXT_COLOR,
+                                 fg_color=OTHER_COLOR)
+server_host_label.pack(padx=10, pady=10)
+
+server_port_frame = ctk.CTkFrame(
+    frame_top, fg_color=OTHER_COLOR, corner_radius=8, border_color=ACCENT_COLOR, border_width=1)
+server_port_frame.pack(side="left", padx=5, pady=10, fill="x", expand=True)
+server_port_label = ctk.CTkLabel(server_port_frame, text="", font=("Arial", 11, "bold"), text_color=TEXT_COLOR,
+                                 fg_color=OTHER_COLOR)
+server_port_label.pack(padx=10, pady=10)
+
+discovery_port_frame = ctk.CTkFrame(
+    frame_top, fg_color=OTHER_COLOR, corner_radius=8, border_color=ACCENT_COLOR, border_width=1)
+discovery_port_frame.pack(side="left", padx=5, pady=10, fill="x", expand=True)
+discovery_port_label = ctk.CTkLabel(discovery_port_frame, text="", font=("Arial", 11, "bold"), text_color=TEXT_COLOR,
+                                    fg_color=OTHER_COLOR)
+discovery_port_label.pack(padx=10, pady=10)
 
 # ----- Layout -----
 frame_left = ctk.CTkFrame(app, width=200, corner_radius=15,
@@ -546,6 +571,13 @@ def log(text):
     log_box.insert("end", text + "\n")
     log_box.configure(state="disabled")
     log_box.see("end")
+
+
+def update_server_info_label():
+    """Update the server info labels with host, server port, and discovery port"""
+    server_host_label.configure(text=f"Host: {SERVER_HOST}")
+    server_port_label.configure(text=f"Server Port: {SERVER_PORT}")
+    discovery_port_label.configure(text=f"Discovery Port: {DISCOVERY_PORT}")
 
 
 def update_user_list():
